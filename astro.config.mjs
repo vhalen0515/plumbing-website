0a5e53c0c632b647533873dbfa7d01261a4df508
Trent Avilla <131209117+vhalen0515@users.noreply.github.com>
2026-07-07 08:36:04 -0700

    Fixed astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // If you deploy to GitHub Pages under a custom domain, set `site` to that
-  // domain (e.g. 'https://www.valleylineplumbing.com') and drop a `public/CNAME`
-  // file containing just the domain name. If you deploy to the default
-  // https://<user>.github.io/<repo>/ path instead, also set `base: '/<repo>/'`.
-  site: 'https://www.valleylineplumbing.com',
+    site: 'https://vhalen0515.github.io',
+    base: '/plumbing-website',
 });
